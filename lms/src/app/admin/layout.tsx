eb45acb5f02203ca2dd5e7986d269ee5d47c3fd5
DipNarayanMahara2 <dipnarayanmahara28@gmail.com>
2025-03-01 13:08:09 +0545

    prevent user login to admin dashboard

--- lms/src/app/admin/layout.tsx
+++ lms/src/app/admin/layout.tsx
@@ -1,11 +1,23 @@
+"use client";
+
 import Dashboard from "@/components/dashboard/dashboard";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
+
 
 function AdminLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return (
-  <Dashboard>
-    {children}
-    </Dashboard>
-  );
+  const { data: session, status } = useSession();
+  useEffect(() => {
+    console.log(status, "STATUS");
+    if (status === "loading") return;
+
+    if (!session || session.user.role !== "admin") {
+      redirect("/");
+    }
+  }, [session, status]);
+
+  return <Dashboard>{children}</Dashboard>;
 }
 
 export default AdminLayout;
